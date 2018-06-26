ea7881303bff257ef1c7fa5de6499a7967c2027e
Jared Ragland <1407834590@mil>
2018-06-26 09:41:07 -0400

    Tried a couple of different ways to get the batch file to run without timing out...unsuccessful. Reverted to the most basic that runs with cached data.
    TODO: find a way to launch this from an icon for ease-of-use.

--- runVIS.bat
+++ runVIS.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\R\R-3.4.2\bin\x64\R.exe" CMD BATCH --vanilla --slave "O:\Data Tool Development\Freezerworks\FreezerVIS\scripts\runFreezerVIS.R"
+"C:\Program Files\R\R-3.4.2\bin\x64\R.exe" CMD BATCH "O:\Data Tool Development\Freezerworks\FreezerVIS\scripts\runFreezerVIS.R"
